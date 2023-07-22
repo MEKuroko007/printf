2e610a72b1bfcb7f05623bf49485119de5c65683
M-ESSALHI <98237827+MEKuroko007@users.noreply.github.com>
2023-07-22 23:35:28 +0100

    Update printf_int.c

--- printf_int.c
+++ printf_int.c
@@ -1,47 +1,57 @@
 #include "main.h"
+
 /**
- * printDigits - recursion function
- * @n:int
- * Return:number of characters
- */
+  * convert_dec - Convert number to decimal and print the digits.
+  * @num: The number to be converted.
+  * @p: The divisor to get the most significant digit.
+  * @i: Pointer to keep track of the number of digits printed.
+  */
+void convert_dec(int num, int p, int *i) {
+    if (p == 0) {
+        return;
+    }
 
-
-int printDigits(int n)
-{
-	if (n == 0)
-	{
-		return (0);
-	}
-	int count = printDigits(n / 10);
-
-	_putchar('0' + n % 10);
-	return (count + 1);
+    int digit = num / p;
+    _putchar(digit + '0');
+    num = num - (digit * p);
+    p = p / 10;
+    (*i)++;
+    convert_dec(num, p, i);
 }
+
 /**
- * print_int - prints integer
- * @args:argument
- * Return:number of characters
- */
-int print_int(va_list args)
-{
-	int num = va_arg(args, int);
-	int count = 0;
+  * print_dec - Print decimal number.
+  * @args: Arguments containing the decimal number to be printed.
+  * Return: The length of the number (number of characters printed).
+  */
+int print_dec(va_list args) {
+    int n = va_arg(args, int);
+    int num, last = n % 10;
+    int i = 1;
+    int p = 1;
 
-	if (num < 0)
-	{
-		_putchar('-');
-		num = -num;
-		count++;
-	}
-	if (num == 0)
-	{
-		_putchar('0');
-		count++;
-	}
-	else
-	{
-		count += printDigits(num);
-	}
+    n = n / 10;
+    num = n;
 
-	return (count);
+    if (last < 0) {
+        _putchar('-');
+        num = -num;
+        n = -n;
+        last = -last;
+        i++;
+    }
+
+    if (num > 0) {
+        while (num / 10 != 0) {
+            p = p * 10;
+            num = num / 10;
+        }
+        num = n;
+        convert_dec(num, p, &i);
+    }
+
+    _putchar(last + '0');
+
+    return i;
 }
+
