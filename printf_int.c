3700dea61afe3c9dede542c47f47eac551fc6838
ouailb <adomeyou@gmail.com>
2023-07-22 21:45:44 +0100

    create new file print_int

---
+++ printf_int.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * printDigits - recursion function
+ * @n:int
+ * Return:number of characters
+ */
+
+
+int printDigits(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    int count = printDigits(n / 10);
+    _putchar('0' + n % 10);
+    return count + 1;
+}
+/**
+ * print_int - prints integer
+ * @args:argument
+ * Return:number of characters
+ */
+int print_int(va_list args)
+{
+  int num = va_arg(args, int);
+  int count = 0;
+  if (num < 0) {
+      _putchar('-');
+      num = -num;
+      count++;
+  }
+  if (num == 0) {
+      _putchar('0');
+      count++;
+  }else{
+      count += printDigits(num);
+  }
+
+    return count;
+}
