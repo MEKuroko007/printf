bd61d21b0268f8676b45aa9e6b062695d678d57a
MEKuroko007 <essalhimu@gmail.com>
2023-07-22 13:37:12 -0700

    fix some errors

--- _strlen.c
+++ _strlen.c
@@ -1,13 +1,18 @@
 #include "main.h"
+/**
+ * _strlen - function to return lenght of char
+ * @s:char
+ * Return:count
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+	
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
 
-int _strlen(char *s){
-    int count = 0;
-
-    while (*s != '\0')
-    {
-        count++;
-        s++;
-    }
-
-    return (count);
+	return (count);
 }
