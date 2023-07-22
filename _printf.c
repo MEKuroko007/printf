a48e7d6c1faf2f265a2499049722d0efa1447e92
MEKuroko007 <essalhimu@gmail.com>
2023-07-22 12:34:39 -0700

    create _printf.c

---
+++ _printf.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * _printf - function to print something
+ * @format:format
+ * Return:length
+ */
+int _printf(const char *format, ...)
+{
+	sp array[] = {
+		{"%c", print_char},
+		{"%s", print_string},
+		{"%%", print_percent},
+	};
+
+	va_list args;
+	int i = 0, j, len = 0;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	return (-1);
+  
+	while (format[i]) 
+	{
+	int found = 0;
+	j = 2;
+		while (j >= 0) {
+			if (array[j].s[0] == format[i] && array[j].s[1] == format[i + 1]) {
+        printf("truuuuuuuuuuuuuuuuuue");
+				len += array[j].f(args);
+				i = i + 2;
+				found = 1;
+				break;
+			}
+			j--;
+		}
+
+		if (!found) {
+			_putchar(format[i]);
+			len++;
+			i++;
+		}
+	}
+
+	va_end(args);
+	return (len);
+}
