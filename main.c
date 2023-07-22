219b6a933c7cd2e79085af8e06e3beaff2353464
M-ESSALHI <98237827+MEKuroko007@users.noreply.github.com>
2023-07-22 23:22:39 +0100

    Update main.c

--- main.c
+++ main.c
@@ -23,5 +23,9 @@ int main(void)
 	'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
 	 _printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
 	printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
+	 _printf("Length:[%d, %i]\n", len, len);
+    	printf("Length:[%d, %i]\n", len2, len2);
+    	_printf("Negative:[%d]\n", -762534);
+    	printf("Negative:[%d]\n", -762534);
 	return (0);
 }
