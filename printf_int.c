733f6194bb4fcd00d21109c3f7f83bfd700fc534
M-ESSALHI <98237827+MEKuroko007@users.noreply.github.com>
2023-07-22 23:52:11 +0100

    Update printf_int.c

--- printf_int.c
+++ printf_int.c
@@ -1,57 +1,46 @@
 #include "main.h"
-
 /**
-  * convert_dec - Convert number to decimal and print the digits.
-  * @num: The number to be converted.
-  * @p: The divisor to get the most significant digit.
-  * @i: Pointer to keep track of the number of digits printed.
-  */
-void convert_dec(int num, int p, int *i) {
-    if (p == 0) {
-        return;
-    }
+ * printf_int - prints integer
+ * @args: argument to print
+ * Return: number of characters printed
+ */
+int print_int(va_list args)
+{
+  printf("hi");
+	int n = va_arg(args, int);
+  printf("%d",n);
+	int num, last = n % 10, digit, exp = 1;
+	int  i = 1;
 
-    int digit = num / p;
-    _putchar(digit + '0');
-    num = num - (digit * p);
-    p = p / 10;
-    (*i)++;
-    convert_dec(num, p, i);
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+
+	return (i);
 }
-
-/**
-  * print_dec - Print decimal number.
-  * @args: Arguments containing the decimal number to be printed.
-  * Return: The length of the number (number of characters printed).
-  */
-int print_dec(va_list args) {
-    int n = va_arg(args, int);
-    int num, last = n % 10;
-    int i = 1;
-    int p = 1;
-
-    n = n / 10;
-    num = n;
-
-    if (last < 0) {
-        _putchar('-');
-        num = -num;
-        n = -n;
-        last = -last;
-        i++;
-    }
-
-    if (num > 0) {
-        while (num / 10 != 0) {
-            p = p * 10;
-            num = num / 10;
-        }
-        num = n;
-        convert_dec(num, p, &i);
-    }
-
-    _putchar(last + '0');
-
-    return i;
-}
-
