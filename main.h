349cecc3f65db8c2d00c6c5f3d0f3c6e1bc63775
MEKuroko007 <essalhimu@gmail.com>
2023-07-22 12:22:28 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <limits.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * struct format - format
+ * @s:char
+ * @f:pointer to function
+ */
+
+typedef struct specifier {
+  char *s;
+  int (*f)();
+} sp;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_string(va_list args);
+int _strlen(char *s);
+int print_char(va_list args);
+int print_percent(void);
+
+
+#endif
