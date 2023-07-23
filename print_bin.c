210954480f86a292f0cf8d721060ad0c7efd63e5
M-ESSALHI <98237827+MEKuroko007@users.noreply.github.com>
2023-07-23 20:10:26 +0100

    Update print_bin.c

--- print_bin.c
+++ print_bin.c
@@ -1,35 +1,41 @@
 #include "main.h"
 /**
- *convert - convert number
- *@num:integer
- *Return:pointer
+ * convert - convert number to binary
+ * @num: integer
+ * @buffer: buffer to store the binary representation
+ * @buffer_size: size of the buffer
+ * Return: pointer to the buffer
  */
-char *convert(int num)
+char *convert(int num, char *buffer, int buffer_size)
 {
-	static char buffer[50];
-	char *ptr;
+    char *ptr = &buffer[buffer_size - 1];
+    *ptr = '\0';
 
-	ptr = &buffer[49];
-	*ptr = '\0';
-	do {
-		*--ptr = '0' + (num % 2);
-		num /= 2;
-	} while (num != 0);
+    do {
+        *--ptr = '0' + (num % 2);
+        num /= 2;
+    } while (num != 0);
 
-	return (ptr);
+    return ptr;
 }
+
 /**
- *print_bin - convert numbers to binary
- *@args:arguments
- *Return:print numbers as string
+ * print_bin - convert numbers to binary and print
+ * @args: arguments
+ * Return: the number of characters printed
  */
 int print_bin(va_list args)
 {
-	int num = va_arg(args, int);
-	char *str;
+    int num = va_arg(args, int);
+    char buffer[50];
+    char *str = convert(num, buffer, sizeof(buffer));
 
-	str = convert(num);
+    int len = 0;
+    while (*str) {
+        _putchar(*str);
+        str++;
+        len++;
+    }
 
-	return (_puts(str));
+    return len;
 }
-
