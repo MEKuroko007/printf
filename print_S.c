9cd5e2146d39cdacd5b419c23c2a29762e42981e
ouailb <adomeyou@gmail.com>
2023-07-24 12:28:54 +0100

    edit betty  print_S

--- print_S.c
+++ print_S.c
@@ -1,26 +1,31 @@
 #include "main.h"
 /**
- * printf_S - non printable character
+ * print_S - non printable character
  * @args:arguments
  * Return:length of string
  */
 
-int print_S(va_list args) {
-    char *s = va_arg(args, char *);
+int print_S(va_list args)
+{
+	char *s = va_arg(args, char *);
 
-    if (!s)
-        s = "(null)";
+	if (!s)
+		s = "(null)";
 
-    int len = 0;
+	int len = 0;
 
-    for (; *s; s++) {
-        if (*s < 32 || *s >= 127) {
-            len += _putchar('\\') + _putchar('x');
-            len += print_HEX(*s);
-        } else {
-            len += _putchar(*s);
-        }
-    }
+	for (; *s; s++)
+	{
+		if (*s < 32 || *s >= 127)
+		{
+			len += _putchar('\\') + _putchar('x');
+			len += print_HEX(*s);
+		}
+		else
+		{
+			len += _putchar(*s);
+		}
+	}
 
-    return len;
+	return (len);
 }
