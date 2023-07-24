1c4d42ea875868e469f4ba65e4784453d6f990eb
MEKuroko007 <essalhimu@gmail.com>
2023-07-24 07:10:25 -0700

    fix missing values and betty styles

--- print_rot13.c
+++ print_rot13.c
@@ -7,24 +7,24 @@
  */
 int print_rot13(va_list args)
 {
-    int i, j;
-    char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-    char *s = va_arg(l, char *);
+	int i, j;
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *s = va_arg(args, char *);
 
-    for (j = 0; s[j]; j++)
-    {
-        if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z')
-            _putchar(s[j]);
-        else
-        {
-            for (i = 0; i <= 52; i++)
-            {
-                if (s[j] == rot13[i])
-                    _putchar(ROT13[i]);
-            }
-        }
-    }
+	for (j = 0; s[j]; j++)
+	{
+		if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z')
+			_putchar(s[j]);
+		else
+		{
+			for (i = 0; i <= 52; i++)
+			{
+				if (s[j] == rot13[i])
+					_putchar(ROT13[i]);
+			}
+		}
+	}
 
-    return j;
+	return (j);
 }
