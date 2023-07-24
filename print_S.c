b1e1ed2d339a2131d5a2b7c493e40b3c173804b8
MEKuroko007 <essalhimu@gmail.com>
2023-07-24 13:47:38 -0700

    fix problems of multi defntions and betty styles :)

--- print_S.c
+++ print_S.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * convertHEX - function to convert number to hexadecimal
- * @c: char
+ * @num: unsigned number to convert hex decimal
  * Return: pointer to the buffer
  */
 char *convertHEX(unsigned int num)
@@ -15,7 +15,7 @@ char *convertHEX(unsigned int num)
 		num /= 16;
 	} while (num != 0 && ptr > buffer);
 
-	return ptr;
+	return (ptr);
 }
 /**
  * print_S - non-printable character
@@ -24,28 +24,28 @@ char *convertHEX(unsigned int num)
  */
 int print_S(va_list args)
 {
-    int i, count = 0;
-    char *res;
-    char *s = va_arg(args, char *);
+	int i, count = 0;
+	char *res;
+	char *s = va_arg(args, char *);
 
-    if (!s)
-        return (_puts("(null)"));
+	if (!s)
+		return (_puts("(null)"));
 
-    for (i = 0; s[i]; i++)
-    {
-        if (s[i] > 0 && (s[i] < 32 || s[i] >= 127))
-        {
-            _puts("\\x");
-            count += 2;
-            res = convertHEX(s[i]);
-            if (!res[1])
-                count += _putchar('0');
-            count += _puts(res);
-        }
-        else
-            count += _putchar(s[i]);
-    }
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] > 0 && (s[i] < 32 || s[i] >= 127))
+		{
+			_puts("\\x");
+			count += 2;
+			res = convertHEX(s[i]);
+			if (!res[1])
+				count += _putchar('0');
+			count += _puts(res);
+		}
+		else
+			count += _putchar(s[i]);
+	}
 
-    return count;
+	return (count);
 }
 
--- print_p.c
+++ print_p.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * convertX - convert number to HEX
+ * convertXP - convert number to HEX
  * @num: integer to be converted
  * @buffer: buffer to store the binary representation
  * @buffer_size: size of the buffer
  * Return: pointer to the buffer
  */
-char *convertX(unsigned int num, char *buffer, int buffer_size)
+char *convertXP(unsigned int num, char *buffer, int buffer_size)
 {
 	char *ptr = &buffer[buffer_size - 1];
 	char *upp =  "0123456789ABCDEF";
@@ -34,7 +34,7 @@ int print_p(va_list args)
 	if (!p)
 		return (_puts("(null)"));
 
-	str = convertX(p, buffer, sizeof(buffer));
+	str = convertXP(p, buffer, sizeof(buffer));
 	count += _puts("0x");
 	count += _puts(str);
 
